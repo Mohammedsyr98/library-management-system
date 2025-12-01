78e6ef1fa1abd6e3e48660d9a4d55d34d9fc4e9f
Mohammmed sayrafi <127789987+Mohammedsyr98@users.noreply.github.com>
2025-12-01 13:57:36 +0300

    Fix type errors

--- components/ToastHandler.tsx
+++ components/ToastHandler.tsx
@@ -1,24 +1,18 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 import { useToast } from "../hooks/useToast";
 import Cookies from "js-cookie";
 export function ToastHandler() {
-  const searchParams = useSearchParams();
+  const toastKey = Cookies.get("toast");
+  const toastType = Cookies.get("toastType") as "error" | "success" | undefined;
   const { showToast } = useToast();
 
   useEffect(() => {
-    const toastKey = Cookies.get("toast");
-    const toastType = Cookies.get("toastType") as
-      | "error"
-      | "success"
-      | undefined;
-
     if (toastKey) {
       showToast(toastKey, toastType || "error");
     }
-  }, [searchParams, showToast]);
+  }, [showToast, toastKey, toastType]);
 
   return null;
 }
