e07fcca2b9b66441bbeb73845f1b4647750d02ed
Mohamed Seirafi <mohammedssyr@gmail.com>
2025-12-24 11:14:45 +0300

    Implement book details page

--- components/website/book-details/SimilarBooks.tsx
+++ components/website/book-details/SimilarBooks.tsx
@@ -1,18 +1,27 @@
+import { getSimilarBooks } from "@/Services/server/services";
 import { getBookImageUrl } from "@/utils";
 import Image from "next/image";
 import Link from "next/link";
 
-const PopularBooks = ({ books }: { books: BookRow[] | null }) => {
-  if (!books) return null;
-
+const SimilarBooks = async ({
+  currentBook,
+}: {
+  currentBook: BookRow | null;
+}) => {
+  if (!currentBook) return null;
+  const { data: books } = await getSimilarBooks(
+    currentBook.genre,
+    currentBook.author,
+    currentBook.id
+  );
   return (
     <section className="mt-12">
       <h2 className="text-white text-2xl sm:text-3xl font-semibold">
-        Popular Books
+        More Similar Books
       </h2>
 
       <div className="mt-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 gap-10">
-        {books.map((book) => (
+        {books?.map((book) => (
           <Link
             href={`/book-details?id=${book.id}`}
             prefetch={true}
@@ -40,4 +49,4 @@ const PopularBooks = ({ books }: { books: BookRow[] | null }) => {
   );
 };
 
-export default PopularBooks;
+export default SimilarBooks;
